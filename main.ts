34c375591c3497e842f0979287881ceae2c391b9
zlq4863947 <zlq4863947@gmail.com>
2018-12-10 10:42:50 +0900

    格式化代码加入main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import * as url from 'url';
 
-import { BrowserWindow, Menu, app, autoUpdater, screen, dialog, shell } from 'electron';
+import { BrowserWindow, Menu, app, autoUpdater, dialog, screen, shell } from 'electron';
 import * as log from 'electron-log';
 import * as settings from 'electron-settings';
 
@@ -17,161 +17,194 @@ function createWindow() {
   const templateMenu = [
     {
       label: '编辑',
-      submenu: [{
-        label: '关于',
-        role: 'about'
-      },{
-        label: '撤销',
-        accelerator: 'CmdOrCtrl+Z',
-        role: 'undo'
-      }, {
-        label: '重做',
-        accelerator: 'Shift+CmdOrCtrl+Z',
-        role: 'redo'
-      }, {
-        type: 'separator'
-      }, {
-        label: '剪切',
-        accelerator: 'CmdOrCtrl+X',
-        role: 'cut'
-      }, {
-        label: '复制',
-        accelerator: 'CmdOrCtrl+C',
-        role: 'copy'
-      }, {
-        label: '粘贴',
-        accelerator: 'CmdOrCtrl+V',
-        role: 'paste'
-      }, {
-        label: '全选',
-        accelerator: 'CmdOrCtrl+A',
-        role: 'selectall'
-      }]
-    }, {
+      submenu: [
+        {
+          label: '关于',
+          role: 'about',
+        },
+        {
+          label: '撤销',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo',
+        },
+        {
+          label: '重做',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: '剪切',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut',
+        },
+        {
+          label: '复制',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy',
+        },
+        {
+          label: '粘贴',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste',
+        },
+        {
+          label: '全选',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall',
+        },
+      ],
+    },
+    {
       label: '查看',
-      submenu: [{
-        label: '重载',
-        accelerator: 'CmdOrCtrl+R',
-        click: function (item, focusedWindow) {
-          if (focusedWindow) {
-            // 重载之后, 刷新并关闭所有的次要窗体
-            if (focusedWindow.id === 1) {
-              BrowserWindow.getAllWindows().forEach(function (win) {
-                if (win.id > 1) {
-                  win.close()
-                }
-              })
+      submenu: [
+        {
+          label: '重载',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              // 重载之后, 刷新并关闭所有的次要窗体
+              if (focusedWindow.id === 1) {
+                BrowserWindow.getAllWindows().forEach(function(win) {
+                  if (win.id > 1) {
+                    win.close();
+                  }
+                });
+              }
+              focusedWindow.reload();
             }
-            focusedWindow.reload()
-          }
-        }
-      }, {
-        label: '切换全屏',
-        accelerator: (function () {
-          if (process.platform === 'darwin') {
-            return 'Ctrl+Command+F'
-          } else {
-            return 'F11'
-          }
-        })(),
-        click: function (item, focusedWindow) {
-          if (focusedWindow) {
-            focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-          }
-        }
-      }, {
-        label: '切换开发者工具',
-        accelerator: (function () {
-          if (process.platform === 'darwin') {
-            return 'Alt+Command+I'
-          } else {
-            return 'Ctrl+Shift+I'
-          }
-        })(),
-        click: function (item, focusedWindow) {
-          if (focusedWindow) {
-            focusedWindow.toggleDevTools()
-          }
-        }
-      }, {
-        type: 'separator'
-      }, {
-        label: '应用程序菜单演示',
-        click: function (item, focusedWindow) {
-          if (focusedWindow) {
-            const options = {
-              type: 'info',
-              title: '应用程序菜单演示',
-              buttons: ['好的'],
-              message: '此演示用于 "菜单" 部分, 展示如何在应用程序菜单中创建可点击的菜单项.'
+          },
+        },
+        {
+          label: '切换全屏',
+          accelerator: (function() {
+            if (process.platform === 'darwin') {
+              return 'Ctrl+Command+F';
+            } else {
+              return 'F11';
             }
-            dialog.showMessageBox(focusedWindow, options, function () {})
-          }
-        }
-      }]
-    }, {
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+            }
+          },
+        },
+        {
+          label: '切换开发者工具',
+          accelerator: (function() {
+            if (process.platform === 'darwin') {
+              return 'Alt+Command+I';
+            } else {
+              return 'Ctrl+Shift+I';
+            }
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              focusedWindow.toggleDevTools();
+            }
+          },
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: '应用程序菜单演示',
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              const options = {
+                type: 'info',
+                title: '应用程序菜单演示',
+                buttons: ['好的'],
+                message: '此演示用于 "菜单" 部分, 展示如何在应用程序菜单中创建可点击的菜单项.',
+              };
+              dialog.showMessageBox(focusedWindow, options, function() {});
+            }
+          },
+        },
+      ],
+    },
+    {
       label: '窗口',
       role: 'window',
-      submenu: [{
-        label: '最小化',
-        accelerator: 'CmdOrCtrl+M',
-        role: 'minimize'
-      }, {
-        label: '关闭',
-        accelerator: 'CmdOrCtrl+W',
-        role: 'close'
-      }, {
-        type: 'separator'
-      }, {
-        label: '重新打开窗口',
-        accelerator: 'CmdOrCtrl+Shift+T',
-        enabled: false,
-        key: 'reopenMenuItem',
-        click: function () {
-          app.emit('activate')
-        }
-      }]
-    }, {
+      submenu: [
+        {
+          label: '最小化',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize',
+        },
+        {
+          label: '关闭',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: '重新打开窗口',
+          accelerator: 'CmdOrCtrl+Shift+T',
+          enabled: false,
+          key: 'reopenMenuItem',
+          click: function() {
+            app.emit('activate');
+          },
+        },
+      ],
+    },
+    {
       label: '帮助',
       role: 'help',
-      submenu: [{
-        label: '学习更多',
-        click: function () {
-          shell.openExternal('http://electron.atom.io')
-        }
-      }]
+      submenu: [
+        {
+          label: '学习更多',
+          click: function() {
+            shell.openExternal('http://electron.atom.io');
+          },
+        },
+      ],
     },
     {
       label: `关于 ${name}`,
-      role: 'about'
-    }, {
-      type: 'separator'
-    }, {
+      role: 'about',
+    },
+    {
+      type: 'separator',
+    },
+    {
       label: '服务',
       role: 'services',
-      submenu: []
-    }, {
-      type: 'separator'
-    }, {
+      submenu: [],
+    },
+    {
+      type: 'separator',
+    },
+    {
       label: `隐藏 ${name}`,
       accelerator: 'Command+H',
-      role: 'hide'
-    }, {
+      role: 'hide',
+    },
+    {
       label: '隐藏其它',
       accelerator: 'Command+Alt+H',
-      role: 'hideothers'
-    }, {
+      role: 'hideothers',
+    },
+    {
       label: '显示全部',
-      role: 'unhide'
-    }, {
-      type: 'separator'
-    }, {
+      role: 'unhide',
+    },
+    {
+      type: 'separator',
+    },
+    {
       label: '退出',
       accelerator: 'Command+Q',
-      click: function () {
-        app.quit()
-      }
-    }
+      click: function() {
+        app.quit();
+      },
+    },
   ];
 
   const menu = Menu.buildFromTemplate(<any>templateMenu);
